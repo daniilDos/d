7671fd9802806ddbc79089db3a190d6c350ec2e9
daniilDos <60820357+daniilDos@users.noreply.github.com>
2020-07-12 14:08:34 +0300

    Add files via upload

---
+++ 14.2.py
@@ -0,0 +1,10 @@
+authrs = {
+    "Charles Dickens": "1870",
+    "William Thackeray": "1863",
+    "Anthony Trollope": "1882",
+    "Gerard Manley Hopkins": "1889" }
+
+for author in authrs:
+    print(author)
+    
+author = input('name')
